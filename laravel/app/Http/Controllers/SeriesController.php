586482b842b98af25e31093a56d3c08869feaad4
Raul Melo <raul.m@aluno.ifsp.edu.br>
2025-10-30 15:24:18 -0300

    criar edicao de serie

--- laravel/app/Http/Controllers/SeriesController.php
+++ laravel/app/Http/Controllers/SeriesController.php
@@ -24,16 +24,27 @@ class SeriesController extends Controller
     public function store(Request $request)
     {
         $serie = Serie::create($request->all()); // use only para buscar apenas os campos desejados e except para ignorar campos específicos
-        $request->session()->flash('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
 
-        return to_route('series.index');
+        return to_route('series.index')->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
     }
 
-    public function destroy(Serie $series, Request $request)
+    public function destroy(Serie $series)
     {
         $series->delete();
-        $request->session()->flash('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso");
  
-        return to_route('series.index');
+        return to_route('series.index')->with('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso");
+    }
+
+    public function edit(Serie $series)
+    {
+        return view('series.edit')->with('serie', $series);
+    }
+
+    public function update(Request $request, Serie $series)
+    {
+        $series->fill($request->all());
+        $series->save();
+
+        return to_route('series.index')->with('mensagem.sucesso', "Série '{$series->nome}' atualizada com sucesso");
     }
 }
